a46d3c58386e14d8bf81adcd1df4ef1217ba631b
SamiChafik <sami.chafik00@gmail.com>
2025-05-15 12:27:44 +0100

    controller

---
+++ src/main/java/com/example/budget_manager/controllers/TransactionController.java
@@ -0,0 +1,43 @@
+package com.example.budget_manager.controllers;
+
+import com.example.budget_manager.entities.Transaction;
+import com.example.budget_manager.repositories.TransactionRepository;
+import com.example.budget_manager.services.TransactionService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class TransactionController {
+
+    @Autowired
+    public final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @PostMapping("/transaction")
+    public Transaction saveTransaction(@RequestBody Transaction transaction) {
+        return transactionService.saveTransaction(transaction);
+    }
+
+    @GetMapping("/transaction")
+    public List<Transaction> saveTransaction() {
+        return transactionService.getAllTransactions();
+    }
+
+
+    @PutMapping("/transaction")
+    public Transaction updateTransaction(@RequestBody Transaction transaction) {
+        return transactionService.updateTransaction(transaction);
+    }
+
+    @DeleteMapping("/transaction/{id}")
+    public void deleteTransaction(@PathVariable Long id) {
+        transactionService.deleteTransaction(id);
+    }
+}
