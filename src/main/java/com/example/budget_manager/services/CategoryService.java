e993e775234bfad7f50fb580f26021d8d8acec20
SamiChafik <sami.chafik00@gmail.com>
2025-05-17 23:11:17 +0100

    added Cetegory service containing all crud funcs

---
+++ src/main/java/com/example/budget_manager/services/CategoryService.java
@@ -0,0 +1,44 @@
+package com.example.budget_manager.services;
+
+import com.example.budget_manager.entities.Category;
+import com.example.budget_manager.entities.Transaction;
+import com.example.budget_manager.repositories.CategoryRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class CategoryService {
+    private CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public Category createCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public Category updateCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Category> getCategoryById(Long id) {
+        return categoryRepository.findById(id);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @Transactional
+    public void deleteCategory(Long id) {
+        categoryRepository.deleteById(id);
+    }
+
+}
