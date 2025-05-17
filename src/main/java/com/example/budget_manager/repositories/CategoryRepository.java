567ca50dd31e9f7168c4379ba86e873cf4e51cf9
SamiChafik <sami.chafik00@gmail.com>
2025-05-17 23:10:49 +0100

    added Cetegory repo

---
+++ src/main/java/com/example/budget_manager/repositories/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.example.budget_manager.repositories;
+
+import com.example.budget_manager.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+
+
+}
