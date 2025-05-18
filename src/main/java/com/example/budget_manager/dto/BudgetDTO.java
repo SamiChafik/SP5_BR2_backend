08b8a7bf29b62612f22c7468cb42754e365ec6f2
SamiChafik <sami.chafik00@gmail.com>
2025-05-18 23:16:26 +0100

    budget DTO

---
+++ src/main/java/com/example/budget_manager/dto/BudgetDTO.java
@@ -0,0 +1,42 @@
+package com.example.budget_manager.dto;
+import lombok.*;
+
+@NoArgsConstructor @AllArgsConstructor
+public class BudgetDTO {
+    private Long id;
+    private String category;
+    private Double limitAmount;
+    private Double spentAmount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Double getLimitAmount() {
+        return limitAmount;
+    }
+
+    public void setLimitAmount(Double limitAmount) {
+        this.limitAmount = limitAmount;
+    }
+
+    public Double getSpentAmount() {
+        return spentAmount;
+    }
+
+    public void setSpentAmount(Double spentAmount) {
+        this.spentAmount = spentAmount;
+    }
+}
